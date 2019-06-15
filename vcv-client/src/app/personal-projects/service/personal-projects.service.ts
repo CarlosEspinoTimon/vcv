c682590c4bd65a30d8068273fa16917cdce1e26a
Carlos Espino Timón <carlosespinotimon@gmail.com>
2019-06-15 23:05:19 +0100

    Feature/contact
    Change some texts

--- vcv-client/src/app/personal-projects/service/personal-projects.service.ts
+++ vcv-client/src/app/personal-projects/service/personal-projects.service.ts
@@ -12,7 +12,7 @@ export class PersonalProjectsService {
             [
                 'In this roject I have created a virtual curriculum vitae where I can show information about me.',
                 'Is developed in Angular 7 and deployed in Google App Engine.',
-                'The main idea of this project is for me to practise Angular, but as was doing it I thought that I could do it in a way that anyone could just fill some information in some files and it will have this virtual cv. I plan to write a post where I can show you how could use this as your CV just changing a few things.'
+                'At first the main reason behind this project was to practise Angular, but as time passed I changed my mind in order to expand its potential, so it will serve as a template for others, that they will only need to fill some information in some files and this will produce a virtual CV. In the future I will write a tutorial post in which I\'ll show what changes you have to do in order to obtain your own CV'
             ],
             'Angular 7',
             'https://github.com/CarlosEspinoTimon/vcv',
@@ -22,8 +22,8 @@ export class PersonalProjectsService {
             'CRM',
             [
                 'This is an example of a Customer Relationship Managment. The backend server is developed in Flask and it uses MySQL as database',
-                'I used docker-compose and Docker to create a development environment easily shareable, so if anyone want to participate it just have to clone the project, start it with just one command (having Docker and docker-compose installed) and start coding.',
-                'I implemented a CI/CD pipeline, using Jenkins to automate all the deploymentment. In this case the Jenkins pipeline run some test and the deploys the server in Google App Engine when changes are made in the master branch.'
+                'I used docker-compose and Docker to create a development environment easily shareable, so if anyone wants to participate it just clone the project, start it with just one command (having Docker and docker-compose installed) and start coding.',
+                'I implemented a CI/CD pipeline, using Jenkins to automate all the deploymentment. In this case the Jenkins pipeline runs some tests and the deploys the server in Google App Engine when changes are made in the master branch.'
             ],
             'Python, Flask, Docker, docker-compose, Jenkins, MySQL',
             'https://github.com/CarlosEspinoTimon/crm_service',
@@ -46,7 +46,7 @@ export class PersonalProjectsService {
                 'This was my first project when I started studing the degree and learnt how to program.',
                 'I remember talking with my wife (a school teacher) and she was telling me about how complicated was sometimes to distribute her class. She needed to have homogeneous groups, taking into account things like the maturity of the child, it capacities or if it were responsible or not.',
                 'I decided to create a program where she would insert her students, giving each one some scores about Autonomy, Behavior and Capacities, then program asked how many groups you want to create and give you the solution.',
-                'The program was written in Java, the language that I used during my degree and with which I learn to program, and it has even some tests writen in Junit'
+                'The program was written in Java, the language that I used during my degree and with which I learnt to program, and it has even some tests writen in Junit'
             ],
             'Java',
             'https://github.com/CarlosEspinoTimon/sortYourClassroom',
